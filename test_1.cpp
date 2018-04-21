cb34d168eadb6a9165f61feb341826bdd2ab5315
LY-forever <31944367+LY-forever@users.noreply.github.com>
2018-04-21 22:46:03 +0800

    test
    addding a test.

---
+++ test_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void main()
+{
+	char *s ="hello zy";
+	int i=strlen(s);
+	cout<<s<<endl;
+	cout<<*s<<endl;
+	cout<<i<<endl;
+//	return 0;
+}
